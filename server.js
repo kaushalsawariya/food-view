3fd15f5b35ba868d9460564ee5d0e4d2a9158ab0
kaushal sawariya <sawariyakaushal3@gmail.com>
2025-11-04 23:58:54 +0530

    updated code

--- server.js
+++ server.js
@@ -7,19 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// import { GoogleGenAI } from "@google/genai";
 
-// const ai = new GoogleGenAI({ apiKey: "AIzaSyBkpSlZowhMk0Zbf4jA1yB2akzOpVNOtbE" });
-
-// async function main() {
-//   const response = await ai.models.generateContent({
-//     model: "gemini-2.5-flash",
-//     contents: "i love u ",
-//   });
-//   console.log(response.text);
-// }
-
-// await main();
 
 app.listen(3000, () => {
   console.log("Server listening at port 3000");
